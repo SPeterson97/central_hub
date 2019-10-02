6a59d264fafe729ca5c2eeb86b312ce5bfdc3a06
Samuel Peterson <samuelpeterson@cl2-wifi-10-215-45-84.wireless.pitt.edu>
2019-10-02 14:04:54 -0400

    Command line sending of data

---
+++ test_database.py
@@ -0,0 +1,70 @@
+from google.cloud import firestore
+import datetime
+from pytz import timezone
+
+'''
+# Add a new document
+print("Open client")
+db = firestore.Client()
+print("Client open")
+doc_ref = db.collection(u'test_data').document(u'10022019_1323')
+print("doc ref")
+doc_ref.set({
+u'angle_data': {"0":10,
+"1":11,
+"2":15,
+"3":1
+},
+u'measured_dist': u'1000',
+u'sensor_id': 2019,
+u'time_stamp': datetime.datetime.now(timezone('US/Eastern'))
+})
+
+# Then query for documents
+
+users_ref = db.collection(u'test_data')
+print("Get docs")
+docs = users_ref.get()
+print("done")
+
+for doc in docs:
+print(u'{} => {}'.format(doc.id, doc.to_dict()))
+'''
+
+
+def main():
+	#Create a database connection
+	db = firestore.Client()
+	
+	#Get name for new document
+	doc = input("Enter the name of the document you'd like to create: ")
+	
+	#Create the reference to the document we want to create or modify
+	doc_ref = db.collection(u'test_data').document(doc)
+	#Get some angle data
+	angle0 = int(input("Enter dummy data for angle 0: "))
+	angle1 = int(input("Enter dummy data for angle 1: "))
+	angle2 = int(input("Enter dummy data for angle 2: "))
+	id = int(input("Enter the sensor id: "))
+	
+	#Set the data object
+	doc_ref.set({
+		u'angle_data': {"0":angle0,
+				"1" : angle1,
+				"2"  :angle2,
+	},
+		u'sensor_id': id,
+		u'time_stamp': datetime.datetime.now(timezone('US/Eastern'))
+	})
+
+	#Add to data to database
+	#users_ref = db.collection(u'test_data')
+
+
+	#docs = users_ref.get()
+
+
+
+if __name__ == "__main__":
+	main()
+
